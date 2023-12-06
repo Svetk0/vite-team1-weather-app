e05b7ddcfa34ea90e999019d4a608ec302231040
svetk0 <sv.solomatnikova@gmail.com>
2023-12-07 00:29:21 +0300

    Сделала общую структуру папок в проекте

---
+++ assets/scripts/tests/jquery-test.js
@@ -0,0 +1,14 @@
+//Тестирование библиотеки Jquery
+
+console.log('Hello weather');
+if (window.jQuery) {
+    // Действия для подключённого jQuery
+    console.log('Happy fun');
+  } else {
+    // Действия для неподключённого jQuery
+    console.log('fail :((');
+}
+
+$("button").click(function () {
+    $("h1").hide(5000);
+});
