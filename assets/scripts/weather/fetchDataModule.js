11adc2f1f35178e1732728d1d9c214c8a0bdfc28
itkatprogrammer <itkatprogrammer@gmail.com>
2023-12-08 13:38:49 +0400

    test slider with button logic/error

--- assets/scripts/weather/fetchDataModule.js
+++ assets/scripts/weather/fetchDataModule.js
@@ -1,42 +1,41 @@
 // fetchDataModule.js
-import * as constants from './constants.js';
-import * as ErrorsModule from './errorsModule.js';
-
+import * as constants from "./constants.js";
+import * as ErrorsModule from "./errorsModule.js";
 
 export async function fetchData(location) {
-    try {
-        let API_URL;
+  try {
+    let API_URL;
 
-        if (location) {
-            const { latitude, longitude } = location;
-            API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=ru&appid=${constants.apiKey}&units=metric`;
-        } else {
-            const city = constants.cityInput.value.trim();
-            if (/^\d+$/.test(city)) {
-                ErrorsModule.showError('Введите корректное название города');
-                return;
-            } else {
-                API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${constants.apiKey}&units=metric`;
-                constants.cityInput.value = '';
-            }
-        }
-        ErrorsModule.hideError();
-
-        const response = await fetch(API_URL);
-        if (!response.ok) {
-            throw new Error('Ошибка получения данных о погоде');
-        }
-
-        const data = await response.json();
-        if (data.cod && data.cod !== 200) {
-            ErrorsModule.showError(`Ошибка: ${data.message}`);
-            return;
-        }
-        console.log('Получены данные о погоде:', data.main.temp);
-
-        return data;
-    } catch (error) {
-        console.error('Ошибка получения данных о погоде:', error.message);
-        ErrorsModule.showError('Произошла ошибка. Пожалуйста, попробуйте еще раз.');
+    if (location) {
+      const { latitude, longitude } = location;
+      API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=ru&appid=${constants.apiKey}&units=metric`;
+    } else {
+      const city = constants.cityInput.value.trim();
+      if (/^\d+$/.test(city)) {
+        ErrorsModule.showError("Введите корректное название города");
+        return;
+      } else {
+        API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${constants.apiKey}&units=metric`;
+        constants.cityInput.value = "";
+      }
     }
+    ErrorsModule.hideError();
+
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error("Ошибка получения данных о погоде");
+    }
+
+    const data = await response.json();
+    if (data.cod && data.cod !== 200) {
+      ErrorsModule.showError(`Ошибка: ${data.message}`);
+      return;
+    }
+    console.log("Получены данные о погоде:", data.main.temp);
+
+    return data;
+  } catch (error) {
+    console.error("Ошибка получения данных о погоде:", error.message);
+    ErrorsModule.showError("Произошла ошибка. Пожалуйста, попробуйте еще раз.");
+  }
 }
