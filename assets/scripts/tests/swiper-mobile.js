a5a3ac9d0d88ca8d98a97e89970109c0dee3d1bb
svetk0 <sv.solomatnikova@gmail.com>
2023-12-12 00:17:08 +0300

    change pics in swiper-mobile

--- assets/scripts/tests/swiper-mobile.js
+++ assets/scripts/tests/swiper-mobile.js
@@ -1,38 +1,4 @@
-//import * as fetchDataModule from '../weather/fetchDataModule.js';
-import * as constants from '../weather/constants.js';
-
-
-// Бесконечный куб
-// let swiper = new Swiper(".mySwiper", {
-
-//     effect: "cube",
-//     grabCursor: true,
-//     cubeEffect: {
-//       shadow: true,
-//       slideShadows: true,
-//       shadowOffset: 20,
-//       shadowScale: 0.94,
-//     },
-//     loop: true,
-
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-// });
-
-
-// Вертикальная бесконечная прокрутка
-// var swiper = new Swiper(".mySwiper", {
-//     direction: "vertical",
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     mousewheel: true,
-//     loop: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });   
+ 
 
 //Карточки
 var swiper = new Swiper(".mySwiper-mobile", {
@@ -47,17 +13,17 @@ const packMedium = document.querySelector('.pack-medium');
 const packLight = document.querySelector('.pack-light');
 const packWarm = document.querySelector('.pack-warm');
 
-const clothesImages = ['1.jpg', '2.jpg', '3.jpg'];
+const clothesImages = ['2.jpg', '3.jpg'];
 const pathDemi = 'assets/images/clothes/solomat-test/0-15/';
 const pathWinter = 'assets/images/clothes/solomat-test/minus15-0/';
 const pathSummer = 'assets/images/clothes/solomat-test/15-25/';
 
 function changePack(pathSeason) {
-    packLight.src = pathSeason + clothesImages[1];
+    packLight.src = pathSeason + clothesImages[0];
     console.log(packLight);
-    packMedium.src = pathSeason + clothesImages[0];
-    console.log(packMedium);
-    packWarm.src = pathSeason + clothesImages[2];
+    // packMedium.src = pathSeason + clothesImages[0];
+    //console.log(packMedium);
+    packWarm.src = pathSeason + clothesImages[1];
     console.log(packWarm);
 }
 function determineSeason(currentTemperature) {
