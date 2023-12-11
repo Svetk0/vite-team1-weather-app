62405b7241ff0f02305127503a77c5e4144ceff6
itkatprogrammer <itkatprogrammer@gmail.com>
2023-12-11 12:55:54 +0400

    add true pics for temperature

--- assets/scripts/clothes/clothes.js
+++ assets/scripts/clothes/clothes.js
@@ -31,13 +31,6 @@ async function handleClick(type) {
     let previousTempa = parseInt(data.main.temp);
     let nextTempa = parseInt(temperatureElement.textContent);
     currentTempa = nextTempa || previousTempa;
-    //temperatureElement.textContent = parseInt(data.main.temp);
-
-    console.log("ПРЕДЫДУЩАЯ ТЕМПЕРАТУРА:", previousTempa);
-    console.log("СЛЕДУЮЩАЯ ТЕМПЕРАТУРА:", nextTempa);
-    console.log("ТЕКУЩАЯ ТЕМПЕРАТУРА:", currentTempa);
-
-    swiperWrapper.innerHTML = "";
 
     // в зависимости от типа нажатой кнопки и показанной температуры, определяем массив изображений
     const imageArray = getImageArray(type, currentTempa);
@@ -54,39 +47,76 @@ async function handleClick(type) {
 }
 
 function getImageArray(type, currentTempa) {
-  const imageForHotLess0 = [
-    "/assets/images/clothes/1.jpg",
-    "/assets/images/clothes/2.jpg",
-    "/assets/images/clothes/3.jpg",
-    "/assets/images/clothes/4.jpg",
-    "/assets/images/clothes/5.jpg",
+  const imageForHotFrom0To15 = [
+    "./assets/images/clothes/pics-slider-desctop/0-15/forHot/1.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forHot/2.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forHot/3.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forHot/4.png",
   ];
-  const imageForHotMore0 = [
-    "/assets/images/clothes/11.jpg",
-    "/assets/images/clothes/12.jpg",
-    "/assets/images/clothes/13.jpg",
-    "/assets/images/clothes/14.jpg",
-    "/assets/images/clothes/15.jpg",
+  const imageForColdFrom0To15 = [
+    "./assets/images/clothes/pics-slider-desctop/0-15/forCold/1.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forCold/2.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forCold/3.png",
+    "./assets/images/clothes/pics-slider-desctop/0-15/forCold/4.png",
   ];
-  const imageForColdMore0 = [
-    "/assets/images/clothes/16.jpg",
-    "/assets/images/clothes/17.jpg",
-    "/assets/images/clothes/18.jpg",
-    "/assets/images/clothes/19.jpg",
-    "/assets/images/clothes/20.jpg",
+  const imageForHotMore16 = [
+    "./assets/images/clothes/pics-slider-desctop/more+15/forHot/1.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forHot/2.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forHot/3.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forHot/4.png",
   ];
-  const imageForColdLess0 = [
-    "/assets/images/clothes/21.jpg",
-    "/assets/images/clothes/22.jpg",
-    "/assets/images/clothes/23.jpg",
-    "/assets/images/clothes/24.jpg",
-    "/assets/images/clothes/25.jpg",
+
+  const imageForColdMore16 = [
+    "./assets/images/clothes/pics-slider-desctop/more+15/forCold/1.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forCold/2.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forCold/4.png",
+    "./assets/images/clothes/pics-slider-desctop/more+15/forCold/5.png",
+  ];
+
+  const imageForHotFromMinus1tillMinus15 = [
+    "./assets/images/clothes/pics-slider-desctop/-14-0/1.jpg",
+    "./assets/images/clothes/pics-slider-desctop/-14-0/2.jpg",
+    "./assets/images/clothes/pics-slider-desctop/-14-0/3.jpg",
+  ];
+
+  const imageForColdFromMinus1tillMinus15 = [
+    "./assets/images/clothes/pics-slider-desctop/-14-0/1.jpg",
+    "./assets/images/clothes/pics-slider-desctop/-14-0/2.jpg",
+    "./assets/images/clothes/pics-slider-desctop/-14-0/3.jpg",
+  ];
+
+  const imageForHotFromMinus16 = [
+    "./assets/images/clothes/pics-slider-desctop/less-15/1.jpg",
+    "./assets/images/clothes/pics-slider-desctop/less-15/2.jpg",
+    "./assets/images/clothes/pics-slider-desctop/less-15/3.jpg",
+  ];
+
+  const imageForColdFromMinus16 = [
+    "./assets/images/clothes/pics-slider-desctop/less-15/1.jpg",
+    "./assets/images/clothes/pics-slider-desctop/less-15/2.jpg",
+    "./assets/images/clothes/pics-slider-desctop/less-15/3.jpg",
   ];
 
   if (type === "hot") {
-    return currentTempa > 0 ? imageForHotMore0 : imageForHotLess0;
+    if (currentTempa >= 0 && currentTempa <= 15) {
+      return imageForHotFrom0To15;
+    } else if (currentTempa > 15) {
+      return imageForHotMore16;
+    } else if (currentTempa <= -1 && currentTempa > -15) {
+      return imageForHotFromMinus1tillMinus15;
+    } else if (currentTempa <= -16) {
+      return imageForHotFromMinus16;
+    }
   } else if (type === "cold") {
-    return currentTempa < 0 ? imageForColdLess0 : imageForColdMore0;
+    if (currentTempa >= 0 && currentTempa <= 15) {
+      return imageForColdFrom0To15;
+    } else if (currentTempa > 15) {
+      return imageForColdMore16;
+    } else if (currentTempa <= -1 && currentTempa > -15) {
+      return imageForColdFromMinus1tillMinus15;
+    } else if (currentTempa <= -16) {
+      return imageForColdFromMinus16;
+    }
   }
 }
 
