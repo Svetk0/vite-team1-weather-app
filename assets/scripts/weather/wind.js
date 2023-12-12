b05bb12d963a89090926318c539f345a508821e7
Lightlana888 <svetlana.yakuta@gmail.com>
2023-12-12 16:34:28 +0100

    yakuta'wind'direction

--- assets/scripts/weather/wind.js
+++ assets/scripts/weather/wind.js
@@ -13,7 +13,7 @@ export function updateWindDirection(degrees, speed) {
     const windDirection = getWindDirection(normalizedDegrees);
 
     constants.windSpeedElement.textContent = `${Math.round(speed)} км/ч`;
-    console.log(`Ветер идет с направлением: ${windDirection}`);
+    console.log(`Ветер идет с направления: ${windDirection}`);
 
-    arrow.style.transform = `translate(-50%, -100%) rotate(${normalizedDegrees}deg)`;
+    arrow.style.transform = `translate(-50%, -100%) rotate(${normalizedDegrees + 180}deg)`;
 }
